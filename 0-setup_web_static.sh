ceead12ce781f4756e960756a7f6c0c36274e293
dtemilade <dtemilade@gmail.com>
2024-01-14 15:28:47 -0800

    completed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 # Sets up a web server for deployment of web_static.
 
-sudo su
+
 apt-get update
 apt-get install -y nginx
 
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-touch /data/web_static/releases/test/index.html
 echo "Simple content to test Nginx configuration" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R $USER:$USER /data/
-chgrp -R $USER:$USER /data/
+
 
 config=\
 "server {
